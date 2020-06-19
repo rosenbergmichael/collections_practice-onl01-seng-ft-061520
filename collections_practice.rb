2a2eeab72d0c56807cb0533132e3ba96aec55db9
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 13:42:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+end 
+end 
+
+
+def sort_array_desc(array)
+array.sort do |a, b|
+ if a == b 
+   0 
+  elsif a < b 
+  1 
+  elsif a > b 
+  -1
+end 
+end 
+end 
+
+
+def sort_array_char_count(array)
+array.sort do |a, b|
+ a.length <=> b.length  
+end 
+end 
+
+def swap_elements(array)
+ array.sort do |a, b|
+ a[1] <=> b[2]
+end 
+end 
+
+
+def reverse_array(array)
+  array.reverse do |a, b|
+    a <=> b
+end 
+end 
+
+
+def kesha_maker(array)
+  array.each do |a|
+    a << "$"
+end 
+end
+
+def find_a(array)
+ 
+end 
+
+
+def sum_array(array)
+  
+end
+
+
+def add_s(array)
+  
+end   
+
 
